5e3591097b0a36acffa9008d9057e4271a966587
lytvynenko <olexiy.lytvynenko@gmail.com>
2025-04-25 16:02:20 +0300

    Amend variables.tf with S3 backend inputs

--- crawl_infrastructure/karpenter/variables.tf
+++ crawl_infrastructure/karpenter/variables.tf
@@ -1,36 +1,49 @@
+##########################
+# EKS Module Inputs
+##########################
+
 variable "cluster_name" {
-  type = string
+  description = "Name of the EKS cluster"
+  type        = string
 }
 
 variable "cluster_endpoint" {
-  type = string
+  description = "EKS cluster endpoint URL"
+  type        = string
 }
 
 variable "oidc_provider_arn" {
-  type = string
+  description = "ARN of the OIDC provider for IAM"
+  type        = string
 }
 
 variable "iam_role_arn" {
-  type = string
+  description = "ARN of the IAM role for node bootstrapping"
+  type        = string
 }
 
 variable "iam_role_name" {
-  type = string
+  description = "Name of the IAM role for node bootstrapping"
+  type        = string
 }
 
 variable "repository_username" {
-  type = string
+  description = "Username for any container registry credentials"
+  type        = string
 }
 
 variable "repository_password" {
-  type = string
+  description = "Password for any container registry credentials"
+  type        = string
 }
 
 variable "karpenter_chart_version" {
-  type = string
+  description = "Helm chart version for Karpenter"
+  type        = string
 }
 
 variable "karpenter_provisioner" {
+  description = "Settings for the Karpenter Provisioner"
   type = object({
     name          = string
     architectures = list(string)
@@ -43,4 +56,30 @@ variable "karpenter_provisioner" {
       effect = string
     }))
   })
+}
+
+##########################
+# Terraform Backend Inputs
+##########################
+
+variable "region" {
+  description = "AWS region where the S3 backend and DynamoDB lock table live"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "state_bucket_name" {
+  description = "Name of the S3 bucket to store Terraform state"
+  type        = string
+}
+
+variable "lock_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locks"
+  type        = string
+}
+
+variable "backend_prefix" {
+  description = "Key prefix in the S3 bucket for this project’s state files (e.g. \"eks\")"
+  type        = string
+  default     = "eks"
 }
