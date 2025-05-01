c019517856a890b2b4d3a70c1ab682a9c5eddd5a
lytvynenko <olexiy.lytvynenko@gmail.com>
2025-05-01 13:21:05 +0300

    Console user auth

--- crawl_infrastructure/auth.tf
+++ crawl_infrastructure/auth.tf
@@ -1,17 +1,26 @@
-# module "aws_auth" {
-#   source                    = "terraform-aws-modules/eks/aws//modules/aws-auth"
+###############################################################################
+# Get the current AWS partition (aws, aws-us-gov, aws-cn)
+###############################################################################
+data "aws_partition" "this" {}
+
+###############################################################################
+# Build the policy ARN for AWSAdministratorAccess
+###############################################################################
+locals {
+  admin_policy_arn = "arn:${data.aws_partition.this.partition}:eks::aws:cluster-access-policy/AWSAdministratorAccess"
+}
+
+###############################################################################
+# EKS Access Entry + Policy Association
+###############################################################################
 resource "aws_eks_access_entry" "console_user" {
   cluster_name  = module.cluster.cluster_name
   principal_arn = "arn:aws:iam::411623750878:user/olexiy"
 }
 
-data "aws_eks_access_policy" "admin" {
-  name = "AWSAdministratorAccess"
-}
-
 resource "aws_eks_access_policy_association" "console_user_admin" {
   cluster_name  = module.cluster.cluster_name
   principal_arn = aws_eks_access_entry.console_user.principal_arn
-  policy_arn = data.aws_eks_access_policy.admin.arn   # <- reuse the looked-up ARN
+  policy_arn = local.admin_policy_arn      # ← use local, not data source
   access_scope { type = "cluster" }
 }
